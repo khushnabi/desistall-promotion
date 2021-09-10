c7cff32e0b531f7866c461c74d30be7da6fedd2c
Khushnabi <p.khushnabi@gmail.com>
2021-09-10 16:07:52 +0530

    video

--- src/components/joining-section/joining-section.js
+++ src/components/joining-section/joining-section.js
@@ -5,9 +5,9 @@ function JoiningSection() {
     <div className="w-full bg-white joining-section">
       <div className="container w-3/4 mx-auto py-16 my-auto flex justify-between">
         <div className="w-1/2">
-            <img src={lady3} className="" alt="Lady 3"/>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/iH1uFtxzu0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-        <div className="w-1/2 pl-16 pt-32">
+        <div className="w-1/2 align-self-center">
             <p className="text-4xl font-semibold leading-tight mb-6">Become a Member</p>
             <p className="text-lg mb-6 w-3/4">Join our exclusive community of blockchain technology enthusiasts by owning a Desi Punk. Share ideas, get help, meet over coffee and build technologies of the future.</p>
             <p className="text-lg mb-6">Mint Price: 0.035 ETH</p>
